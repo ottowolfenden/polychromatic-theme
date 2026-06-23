61af2f0c08513c04e7046ce922791b6fcbc7ed1c
Otto Wolfenden <ottowolfenden@gmail.com>
2026-06-23 17:37:02 +0100

    refine colours in light and dark theme

--- extension.js
+++ extension.js
@@ -8,8 +8,8 @@ function activate(context) {
     }, vscode.ConfigurationTarget.Global);
     config.update("custom-ui-style.stylesheet", {
         ".monaco-editor, .monaco-editor .margin, .monaco-editor-background, .editor-container, .editor-instance, .split-view-view, .tabs-container, .title, .breadcrumbs-widget, .sidebar, .activitybar, .statusbar": "background-color: transparent !important;",
-        ".monaco-workbench.vs-dark": "background-color: #000000cc !important;",
-        ".monaco-workbench.vs": "background-color: #ffffffcc !important;"
+        ".monaco-workbench.vs-dark": "background-color: #000000d6 !important;",
+        ".monaco-workbench.vs": "background-color: #ffffffd6 !important;"
     }, vscode.ConfigurationTarget.Global);
     config.update("workbench.colorTheme", "Polychromatic Theme Dark", vscode.ConfigurationTarget.Global);
     config.update("workbench.preferredDarkColorTheme", "Polychromatic Theme Dark", vscode.ConfigurationTarget.Global);
