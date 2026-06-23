96d1be5e0597e9e5c5ee26127b4a9330823bc414
Otto Wolfenden <ottowolfenden@gmail.com>
2026-06-23 02:25:47 +0100

    package with transparency added again

--- extension.js
+++ extension.js
@@ -2,10 +2,10 @@ const vscode = require("vscode");
 
 function activate(context) {
     const config = vscode.workspace.getConfiguration();
-    // config.update("custom-ui-style.electron", {
-    //     "backgroundColor": "#00000000",
-    //     "transparent": true
-    // }, vscode.ConfigurationTarget.Global);
+    config.update("custom-ui-style.electron", {
+        "backgroundColor": "#00000000",
+        "transparent": true
+    }, vscode.ConfigurationTarget.Global);
     config.update("custom-ui-style.stylesheet", {
         ".monaco-editor, .monaco-editor .margin, .monaco-editor-background, .editor-container, .editor-instance, .split-view-view, .tabs-container, .title, .breadcrumbs-widget, .sidebar, .activitybar, .statusbar": "background-color: transparent !important;",
         ".monaco-workbench.vs-dark": "background-color: #000000cc !important;",
