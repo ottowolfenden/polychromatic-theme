ebab1ce182d41291d5b76b04d7f1ee8828c50815
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-06 05:21:44 +0100

    rename themes and add file icon theme to package.json

--- extension.js
+++ extension.js
@@ -2,10 +2,11 @@ const vscode = require("vscode");
 module.exports = {
     activate: () =>
         Object.entries({
-            "workbench.colorTheme": "Polychromatic Theme Dark",
-            "workbench.preferredDarkColorTheme": "Polychromatic Theme Dark",
-            "workbench.preferredLightColorTheme": "Polychromatic Theme Light",
-            "workbench.productIconTheme": "Material Symbols"
+            "workbench.colorTheme": "polychromatic-dark-colour-theme",
+            "workbench.preferredDarkColorTheme": "polychromatic-dark-colour-theme",
+            "workbench.preferredLightColorTheme": "polychromatic-light-colour-theme",
+            "workbench.iconTheme": "polychromatic-file-icon-theme",
+            "workbench.productIconTheme": "polychromatic-product-icon-theme"
         }).forEach(([key, val]) =>
             vscode.workspace.getConfiguration().update(key, val, vscode.ConfigurationTarget.Global)
         )
